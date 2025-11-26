4c1706486cacc46a20a01d6b3c89b73c270e6bfa
Taimoor Alam <alamtaimoor.de@gmail.com>
2025-11-26 15:39:15 +0100

    fix(layer6): Add proper module exports for CV generator

--- src/layer6/__init__.py
+++ src/layer6/__init__.py
@@ -1,5 +1,6 @@
 # Layer 6: Outreach & CV Generator
 
 from src.layer6.outreach_generator import outreach_generator_node
+from src.layer6.generator import generator_node, MarkdownCVGenerator
 
-__all__ = ["outreach_generator_node"]
+__all__ = ["outreach_generator_node", "generator_node", "MarkdownCVGenerator"]
